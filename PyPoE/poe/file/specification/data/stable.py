a143a3450750a046c1051fb23afbc65f3846d8dd
angelic_knight <angelic.knight009@gmail.com>
2021-11-09 23:20:32 -0600

    Filling in missing content from my /patches-maps branch

--- PyPoE/poe/file/specification/data/stable.py
+++ PyPoE/poe/file/specification/data/stable.py
@@ -14440,6 +14440,18 @@ specification = Specification({
                 name='HeistTier',
                 type='int',
             ),
+            Field(
+                name='RitualTier',
+                type='int',
+            ),
+            Field(
+                name='ExpeditionTier',
+                type='int',
+            ),
+            Field(
+                name='HellscapeTier', #AKA Scourge
+                type='int',
+            ),
         ),
     ),
     'MapStatConditions.dat': File(
